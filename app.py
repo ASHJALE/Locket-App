e9f4e2a82684a8fdb56c69a9f99f7c68d40d738c
ASHJALE <delrosarioashleejale@gmail.com>
2026-02-21 02:42:37 +0800

    Fix port binding for Render

--- app.py
+++ app.py
@@ -63,5 +63,8 @@ def delete(filename):
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)

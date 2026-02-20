9c6f136556dc5394047beabced7527bfad4a7c5e
ASHJALE <delrosarioashleejale@gmail.com>
2026-02-21 03:45:50 +0800

    last1

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ def uploaded_file(filename):
 
 import os
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render assigns the port
+    app.run(host="0.0.0.0", port=port)        # Must bind to all interfaces
